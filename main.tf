3796fd71da67f84dd8a92d53072dae26c8605fda
ivan.sigaev <ivan.sigaev@check24.de>
2024-10-12 21:10:57 +0200

    feat: change condition for gar

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "aws_iam_role" "github_actions_role" {
         Action    = "sts:AssumeRoleWithWebIdentity"
         Condition = {
           StringEquals = {
-            "token.actions.githubusercontent.com:sub" : "repo:woodo01/rsschool-devops-course-tasks:ref:refs/heads/main"
+            "token.actions.githubusercontent.com:sub" : "repo:woodo01/rsschool-devops-course-tasks:*"
           },
           "StringEquals" = {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
