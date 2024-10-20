bea8f5282cdee8c97a69af04ce118917ad4668f9
ivan.sigaev <ivan.develop@gmail.com>
2024-10-20 21:02:39 +0200

    feat: add K8s cluster

--- security.tf
+++ security.tf
@@ -79,3 +79,26 @@ resource "aws_security_group" "private" {
     Name = "Private Group"
   }
 }
+
+resource "aws_security_group" "k3s" {
+  vpc_id      = aws_vpc.root.id
+  name        = "ks3"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "k3s Group"
+  }
+}
